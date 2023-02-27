dc636124cdf1055b189bd14497ddf62317d675c5
amanuel <amanuelibanos@gmail.com>
2023-02-27 12:07:35 -0800

    task 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset-to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input
+ * @n: point to an int 
  * Return: Nothing
  */
 void reset_to_98(int *n);
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap variable value
+ * @a: pointera
+ * @b: pointerb
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+{
+	int m = *a;
+
+	*a = *b;
+	*b = m;
+}
